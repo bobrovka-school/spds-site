5e94858550c10028b83cd02ecfdbfb2b87f127f3
ProjectSoft <projectsoft2009@yandex.ru>
2024-11-19 14:47:04 +0400

    update

--- site/assets/snippets/ogImage/snippet.ogImage.php
+++ site/assets/snippets/ogImage/snippet.ogImage.php
@@ -8,40 +8,16 @@ $out = "";
 if(is_file(MODX_BASE_PATH . $img)):
 	$og_1 = $modx->runSnippet('thumb', array(
 		'input'		=> $img,
-		'options'	=> 'w=640,h=320,f=jpg,zc=C'
-	));
-	/*$og_2 = $modx->runSnippet('thumb', array(
-		'input'		=> $img,
-		'options'	=> 'w=537,h=240,f=jpg,zc=C'
-	));*/
-	$og_3 = $modx->runSnippet('thumb', array(
-		'input'		=> $img,
-		'options'	=> 'w=400,h=400,f=jpg,zc=C'
+		'options'	=> 'w=510,h=300,f=jpg,zc=C'
 	));
 	$out = '
 		<meta itemprop="image" content="' . $og_1 . '">';
 	$out .= '
 		<meta property="og:image" content="' . $og_1 . '">';
 	$out .= '
-		<meta property="og:image:width" content="640">';
+		<meta property="og:image:width" content="510">';
 	$out .= '
-		<meta property="og:image:height" content="320">';
-	$out .= '
-		<meta property="og:image:type" content="image/jpeg">';
-	/*$out .= '
-		<meta property="og:image" content="' . $og_2 . '">';
-	$out .= '
-		<meta property="og:image:width" content="537">';
-	$out .= '
-		<meta property="og:image:height" content="240">';
-	$out .= '
-		<meta property="og:image:type" content="image/jpeg">';*/
-	$out .= '
-		<meta property="og:image" content="' . $og_3 . '">';
-	$out .= '
-		<meta property="og:image:width" content="400">';
-	$out .= '
-		<meta property="og:image:height" content="400">';
+		<meta property="og:image:height" content="300">';
 	$out .= '
 		<meta property="og:image:type" content="image/jpeg">';
 	return $out;
